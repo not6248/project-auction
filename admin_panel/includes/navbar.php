952d9d53c3f5b9a5c36318b9f056b09345ae4078
not6248 <96365700+not6248@users.noreply.github.com>
2023-07-08 12:18:56 +0700

    แก้ไข nav bar เล็กน้อย

--- admin_panel/includes/navbar.php
+++ admin_panel/includes/navbar.php
@@ -11,7 +11,7 @@
       <a href="#" class="nav-link">Contact</a>
     </li>
     <li class="nav-item d-none d-sm-inline-block">
-      <a href="../" class="nav-link">BC Game Store</a>
+      <a href="../" class="nav-link">Go back to test page</a>
     </li>
   </ul>
 
