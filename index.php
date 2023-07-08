c4f201f305dba9fd24d2fab8641f71607ebc979d
not6248 <96365700+not6248@users.noreply.github.com>
2023-07-08 12:19:51 +0700

    Add a test button

--- index.php
+++ index.php
@@ -3,6 +3,11 @@
 <?php include 'includes/head.php'; ?>
 <body>
   <h1>Hello World</h1>
-  <?php include 'includes/script.php'; ?>
+  <?php include 'includes/scripts.php'; ?><br>
+  <a class="btn btn-primary" href="admin_panel">Admin</a>
+  <a class="btn btn-primary" href="admin_panel">Login</a>
+  <a class="btn btn-primary" href="admin_panel">User Page</a>
+
+  
 </body>
 </html>
