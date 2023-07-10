4b49cd41afa76763ba9e0e983442782ad73ea922
not6248 <96365700+not6248@users.noreply.github.com>
2023-07-10 19:08:41 +0700

    Update db_conn.php

--- db/db_conn.php
+++ db/db_conn.php
@@ -11,6 +11,6 @@ $conn = mysqli_connect($servername, $username, $password,$dbname);
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }else
-  echo "Connected successfully";
+  // echo "Connected successfully";
 ?>
 
