52b7a346ff76d071c6300f80547bf62402c83823
not6248 <96365700+not6248@users.noreply.github.com>
2023-07-10 19:09:04 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 <html lang="en">
 <?php include 'includes/head.php'; ?>
 <body>
-  <h1>Hello World</h1>
-  <?php include 'includes/scripts.php'; ?><br>
+  <h1>สมัครเกย์</h1><br>
   <a class="btn btn-primary" href="admin_panel">Admin</a>
   <a class="btn btn-primary" href="login.php">Login</a>
   <a class="btn btn-primary" href="admin_panel">User Page</a>
+  <?php include 'includes/scripts.php'; ?>
 </body>
 </html>
