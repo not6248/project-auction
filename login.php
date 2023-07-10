1b011f5cff78f2081eeded6854cab35c078e9699
not6248 <96365700+not6248@users.noreply.github.com>
2023-07-10 19:08:30 +0700

    Update login.php

--- login.php
+++ login.php
@@ -4,21 +4,31 @@
 <?php include 'includes/head.php'; ?>
 
 <body>
-    <form action="">
-        <div class="main text-center">
-            <div class="loginbox mx-auto mt-5 w-25 p-5 bg-light border border-2 rounded">
-                <h1 class="mb-5">ADMIN Panel</h1>
-                <div class="input-group mb-3">
-                    <input class="form-control" type="email" placeholder="Email">
+    <section class="position-relative py-4 py-xl-5">
+        <div class="container">
+            <div class="row mb-5"></div>
+            <div class="row d-flex justify-content-center">
+                <div class="col-md-6 col-xl-4">
+                    <div class="card mb-5">
+                        <div class="card-body d-flex flex-column align-items-center">
+                            <div class="bs-icon-xl bs-icon-circle bs-icon-primary bs-icon my-4"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-person">
+                                    <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"></path>
+                                </svg></div>
+                            <h2>Admin Penal</h2>
+                            <form class="text-center" method="post">
+                                <div class="mb-3"><input class="form-control" type="email" name="email" placeholder="Email"></div>
+                                <div class="mb-3"><input class="form-control" type="password" name="password" placeholder="Password"></div>
+                                <div class="mb-3"><button class="btn btn-primary d-block w-100" type="submit">Login</button></div>
+                            </form>
+                        </div>
+                    </div>
                 </div>
-                <div class="input-group mb-3">
-                    <input class="form-control" type="password" placeholder="Password">
-                </div>
-                <button class="btn bg-success rounded border text-white mt-3">Log in</button>
             </div>
         </div>
-    </form>
+    </section>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
     <a class="btn btn-primary" href="index.php">Back to test page</a>
+    <?php include 'includes/scripts.php'; ?>
 </body>
 
 </html>
