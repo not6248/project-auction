c44600ce6a3df553099388b12f2692f3b773ebd2
not6248 <96365700+not6248@users.noreply.github.com>
2023-07-12 23:47:29 +0700

    แก้ไขลิงค์ปุ่ม

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 <html lang="en">
 <?php include 'includes/head.php'; ?>
 <body>
-  <h1>สมัครเกย์</h1><br>
+  <h1>ทดสอบ</h1><br>
   <a class="btn btn-primary" href="admin_panel">Admin</a>
-  <a class="btn btn-primary" href="login.php">Login</a>
+  <a class="btn btn-primary" href="admin_panel/login.php">Admin Login</a>
   <a class="btn btn-primary" href="admin_panel">User Page</a>
   <?php include 'includes/scripts.php'; ?>
 </body>
