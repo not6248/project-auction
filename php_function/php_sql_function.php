376431670f4ac0708b5040f28a9bfc24e9f9b362
not6248 <96365700+not6248@users.noreply.github.com>
2023-07-23 23:46:09 +0700

    Update php_sql_function.php

--- php_function/php_sql_function.php
+++ php_function/php_sql_function.php
@@ -1,12 +1,3 @@
 <?php
-function InsertData($conn,$strTable, $strField, $strValue){
-    $strSQL = "INSERT INTO $strTable ($strField) VALUES ($strValue) ";
-    $result = mysqli_query($conn, $strSQL);
-    
-    if ($result) {
-        return true;
-    } else {
-        return false;
-    }
-}
+include "../db/db_conn.php";
 ?>
