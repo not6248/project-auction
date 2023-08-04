d91a5a0df655cc111e24d81d19427f84cce49f65
not6248 <96365700+not6248@users.noreply.github.com>
2023-08-04 16:33:09 +0700

    Update db_conn.php

--- db/db_conn.php
+++ db/db_conn.php
@@ -1,16 +1,15 @@
-<?php
+<?php 
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "auction_pmn";
 
-// Create connection
-$conn = mysqli_connect($servername, $username, $password,$dbname);
+$conn = mysqli_connect($servername,$username,$password,$dbname);
 
 // Check connection
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
-}else
+}
   // echo "Connected successfully";
 ?>
 
