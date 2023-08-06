c38e0b2c59835907984d0a55cca15c5050f5dc22
not6248 <96365700+not6248@users.noreply.github.com>
2023-08-07 02:41:34 +0700

    Create register.php

---
+++ page/register.php
@@ -0,0 +1,32 @@
+<main id="page-container" class="form-signin w-100 m-auto mt-1">
+    <div class="container">
+        <form>
+            <h1 class="h3 mt-5 mb-3 fw-normal">Register Form</h1>
+
+            <div class="form-floating mb-2">
+                <input type="text" class="form-control" id="floatingInput" placeholder="Username">
+                <label for="floatingInput">Username</label>
+            </div>
+            <div class="form-floating mb-2">
+                <input type="email" class="form-control" id="floatingInput" placeholder="Email address">
+                <label for="floatingInput">Email address</label>
+            </div>
+            <div class="form-floating mb-2">
+                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                <label for="floatingPassword">Password</label>
+            </div>
+            <div class="form-floating mb-3">
+                <input type="password" class="form-control" id="floatingPassword" placeholder="Confirm Password">
+                <label for="floatingPassword">Confirm Password</label>
+            </div>
+
+            <!-- <div class="form-check text-start my-3">
+                <input class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault">
+                <label class="form-check-label" for="flexCheckDefault">
+                    Remember me
+                </label>
+            </div> -->
+            <button class="btn btn-primary w-100 py-2" type="submit">Sign Up</button>
+        </form>
+    </div>
+</main>
