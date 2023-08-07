d7acf1f8e898b68a29d07ddb2afccf2d699eaba9
not6248 <96365700+not6248@users.noreply.github.com>
2023-08-08 01:07:54 +0700

    เพิ่ม session_start

--- includes/head.php
+++ includes/head.php
@@ -1,5 +1,8 @@
 <head>
-    <?php require 'db/db_conn.php'; ?>
+    <?php
+    require 'db/db_conn.php';
+    session_start();
+    ?>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
