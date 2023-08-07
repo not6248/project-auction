df23bdff2363e292a68844890e43f592280fcf8c
not6248 <96365700+not6248@users.noreply.github.com>
2023-08-08 01:08:25 +0700

    ปรับปรุงแก้ไขชื่อ input

--- page/register.php
+++ page/register.php
@@ -1,25 +1,32 @@
 <main id="page-container" class="form-signin w-100 m-auto mt-1">
+    <?php if (isset($_SESSION['error'])) : ?>
+        <div class="alert alert-danger" role="alert">
+            <?= $_SESSION['error'];
+            unset($_SESSION['error']); ?>
+        </div>
+    <?php endif; ?>
+
     <div class="container">
         <div class="card-body p-md-5">
             <div class="row justify-content-center">
                 <div class="col-6">
-                    <form>
+                    <form action="page/register_db.php" method="post">
                         <h1 class="text-center h2 fw-bold mb-5 mt-4 fw-normal">Register</h1>
 
                         <div class="form-floating mb-2">
-                            <input type="text" class="form-control" id="floatingInput" placeholder="Username">
+                            <input name="username" type="text" class="form-control" id="floatingInput" placeholder="Username">
                             <label for="floatingInput">Username</label>
                         </div>
                         <div class="form-floating mb-2">
-                            <input type="email" class="form-control" id="floatingInput" placeholder="Email address">
+                            <input name="email" type="email" class="form-control" id="floatingInput" placeholder="Email address">
                             <label for="floatingInput">Email address</label>
                         </div>
                         <div class="form-floating mb-2">
-                            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                            <input name="password" type="password" class="form-control" id="floatingPassword" placeholder="Password">
                             <label for="floatingPassword">Password</label>
                         </div>
                         <div class="form-floating mb-3">
-                            <input type="password" class="form-control" id="floatingPassword" placeholder="Confirm Password">
+                            <input name="c_password" type="password" class="form-control" id="floatingPassword" placeholder="Confirm Password">
                             <label for="floatingPassword">Confirm Password</label>
                         </div>
 
@@ -29,11 +36,11 @@
                     Remember me
                 </label>
             </div> -->
-                        <button class="btn btn-primary w-100 py-2" type="submit">Register</button>
+                        <button name="signup" class="btn btn-primary w-100 py-2" type="submit">Register</button>
                     </form>
                 </div>
-                <div class="col-6 d-flex align-items-center">
-                    <img src="https://static.thairath.co.th/media/dFQROr7oWzulq5Fa5nRRVgnzYSSwUoPM7rigVHaj4QhdURLfyt90hBPNzf89n8vZ5bp.jpg" class="img-fluid" alt="Sample image">
+                <div class="col-6 d-flex align-items-end">
+                    <img src="img/regis_img.jpg" class="img-fluid" alt="Sample image">
                 </div>
 
             </div>
