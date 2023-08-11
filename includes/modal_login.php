14fae8fe8504aac1bbc12cfae7db498645ab1448
not6248 <96365700+not6248@users.noreply.github.com>
2023-08-12 05:20:06 +0700

    สร้างหน้า modal ของการ login

---
+++ includes/modal_login.php
@@ -0,0 +1,23 @@
+<div class="modal-content rounded-4 shadow">
+    <div class="modal-header p-5 pb-4 border-bottom-0">
+        <h1 class="fw-bold mb-0 fs-2">Login</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    </div>
+
+    <div class="modal-body p-4 pb-2 pt-0">
+        <form class="" data-bitwarden-watching="1">
+            <div class="form-floating mb-3">
+                <input type="email" class="form-control rounded-3" id="floatingInput" placeholder="name@example.com">
+                <label for="floatingInput">Email address</label>
+            </div>
+            <div class="form-floating mb-3">
+                <input type="password" class="form-control rounded-3" id="floatingPassword" placeholder="Password">
+                <label for="floatingPassword">Password</label>
+                <p class="text-end mt-1">Forgot <a href="#" class="text-primary">Password?</a></p>
+            </div>
+
+            <button class="w-100 mb-2 btn btn-lg rounded-3 btn-primary" type="submit">Login</button>
+            <p class="text-center mt-2">not a member? <a href="?page=register" class="text-primary">Sign up</a> now</p>
+        </form>
+    </div>
+</div>
