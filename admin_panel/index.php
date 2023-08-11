1cc945eb86308ca8c810e6a8d7e23432923b0637
not6248 <96365700+not6248@users.noreply.github.com>
2023-08-12 05:23:32 +0700

    Update index.php

--- admin_panel/index.php
+++ admin_panel/index.php
@@ -1,35 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php include 'includes/head.php';?>
+<?php include 'includes/head.php'; ?>
+
 <body class="hold-transition sidebar-mini">
-<div class="wrapper">
+  <div class="wrapper">
 
-  <!-- Navbar -->
-  <?php include 'includes/navbar.php';?>
-  <!-- /.navbar -->
+    <!-- Navbar -->
+    <?php include 'includes/navbar.php'; ?>
+    <!-- /.navbar -->
 
-  <!-- Main Sidebar Container -->
-  <?php include 'includes/sidebar.php'; ?>
+    <!-- Main Sidebar Container -->
+    <?php include 'includes/sidebar.php'; ?>
 
-  <!-- Content Wrapper. Contains page content -->
-  <?php
-  if(!isset($_GET['page']) && empty($_GET['page'])){
-    include './page/dashboard/index.php';
-  }elseif((isset($_GET['page']) && $_GET['page'] == 'product')){
-    include './page/product/index.php';
-  }
-  else{
-    include './page/not_found.php';
-  };
-  ?>
-  <!-- /.content-wrapper -->
+    <!-- Content Wrapper. Contains page content -->
+    <?php
+    if (!isset($_GET['page']) && empty($_GET['page'])) {
+      include './page/dashboard/index.php';
+    } elseif ((isset($_GET['page']) && $_GET['page'] == 'product')) {
+      include './page/product/index.php';
+    } else {
+      include './page/not_found.php';
+    };
+    ?>
+    <!-- /.content-wrapper -->
 
-  <!-- Main Footer -->
-  <?php include 'includes/footer.php'?>
-</div>
-<!-- ./wrapper -->
+    <!-- Main Footer -->
+    <?php include 'includes/footer.php' ?>
+  </div>
+  <!-- ./wrapper -->
 
-<!-- REQUIRED SCRIPTS -->
-  <?php include 'includes/scripts.php'?>
+
+
+  <!-- REQUIRED SCRIPTS -->
+  <?php include 'includes/scripts.php' ?>
 </body>
+
 </html>
