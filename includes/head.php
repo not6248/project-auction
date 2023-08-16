49a999f29cb9d350ae168cf8882e152aaae7c95f
not6248 <96365700+not6248@users.noreply.github.com>
2023-08-17 00:17:20 +0700

    เพิ่ม css js

--- includes/head.php
+++ includes/head.php
@@ -8,20 +8,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 
+<!-- CSS -->
     <!-- bootstrap CSS =========================================================-->
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <!-- ------------------------------------------------------------------------->
 
     <!-- sweetalert2 CSS =======================================================-->
     <link rel="stylesheet" href="assets/css/sweetalert2.min.css">
-    <!-- ------------------------------------------------------------------------->
-
-    <!-- jquery ================================================================-->
-    <script src="assets/js/jquery-3.7.0.min.js"></script>
-    <!-- ------------------------------------------------------------------------->
-
-    <!-- sweetalert2 ===========================================================-->
-    <script src="assets/js/sweetalert2.all.min.js"></script>
     <!-- ------------------------------------------------------------------------->
 
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Berkshire+Swash&amp;display=swap">
@@ -34,8 +27,26 @@
     <link rel="stylesheet" href="admin_panel/plugins/fontawesome-free/css/all.min.css">
     <!-- ------------------------------------------------------------------------->
 
+    <!-- datatables =============================================================-->
+    <link rel="stylesheet" href="ssets/css/datatables.min.css">
+    <!-- ------------------------------------------------------------------------->
+
     <!-- main CSS ==============================================================-->
     <link rel="stylesheet" href="assets/css/main.css">
     <!-- ------------------------------------------------------------------------->
+<!-- -->
 
+<!--// Java Script -->
+    <!-- jquery ================================================================-->
+    <script src="assets/js/jquery-3.7.0.min.js"></script>
+    <!-- ------------------------------------------------------------------------->
+
+    <!-- sweetalert2 ===========================================================-->
+    <script src="assets/js/sweetalert2.all.min.js"></script>
+    <!-- ------------------------------------------------------------------------->
+
+    <!-- datatables ============================================================-->
+    <script src="assets/js/datatables.min.js"></script>
+    <!-- ------------------------------------------------------------------------->
+<!-- -->
 </head>
