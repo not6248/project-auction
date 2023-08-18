aeaf7ed493e24ad0ca34302395dedb573e144adc
not6248 <96365700+not6248@users.noreply.github.com>
2023-08-18 16:37:31 +0700

    แก้ไข css

--- admin_panel/login.php
+++ admin_panel/login.php
@@ -2,16 +2,20 @@
 <html lang="en">
 
 <head>
-
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <?php include 'includes/link.php'; ?>
-    <style>
-        * {
-            font-family: 'Kanit', sans-serif;
-        }
-    </style>
+    <!-- main CSS ================================================================================-->
+        <link rel="stylesheet" href="dist/css/main.css">
+    <!-- ------------------------------------------------------------------------------------------->
+
+    <!-- Font Awesome Icons ======================================================================-->
+    <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+    <!-- ------------------------------------------------------------------------------------------->
+
+    <!-- Theme style =============================================================================-->
+    <link rel="stylesheet" href="dist/css/adminlte.min.css">
+    <!-- ------------------------------------------------------------------------------------------->
 </head>
 
 <body>
