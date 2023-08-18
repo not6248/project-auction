5f18b4ebe42c48c89be5704cb5679f0dc31cc877
not6248 <96365700+not6248@users.noreply.github.com>
2023-08-18 16:36:09 +0700

    จัด css และ js ใหม่

--- includes/scripts.php
+++ includes/scripts.php
@@ -1,3 +1,4 @@
-<script 
-src="assets/bootstrap/js/bootstrap.bundle.min.js">
-</script>
+<!-- bootstrap -->
+    <script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- sweetalert2 -->
+    <script src="assets/js/sweetalert2.all.min.js"></script>
