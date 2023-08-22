22ba7a00b2dfd49b1367895a527b281cfa3705f0
not6248 <96365700+not6248@users.noreply.github.com>
2023-08-22 16:44:09 +0700

    เพิ่ม session สำหรับการเช็ค v_code

--- page/login/login_db.php
+++ page/login/login_db.php
@@ -1,53 +1,55 @@
-<?php 
+<?php
 session_start();
 include '../../db/db_conn.php';
 
 
-    $email  = $_POST['email'];
-    $password = $_POST['password'];
+$email  = $_POST['email'];
+$password = $_POST['password'];
 
-    $stmt = mysqli_prepare($conn, "SELECT * FROM login WHERE user_email = ?");
-    mysqli_stmt_bind_param($stmt, "s", $email);
-    if(mysqli_stmt_execute($stmt)){
-        $result = mysqli_stmt_get_result($stmt);
+$stmt = mysqli_prepare($conn, "SELECT * FROM login WHERE user_email = ?");
+mysqli_stmt_bind_param($stmt, "s", $email);
+if (mysqli_stmt_execute($stmt)) {
+    $result = mysqli_stmt_get_result($stmt);
 
-        if(mysqli_num_rows($result) > 0){
-            $row = mysqli_fetch_assoc($result);
-            $row_pass = $row['password'];
-            if(password_verify($password, $row_pass)){
-                // $_SESSION['email'] = $email;
-                $verified_status = $row['email_verified_status'];
-                if($verified_status == '1'){
-                    echo "ยันยัน Mail แล้ว";
+    if (mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+        $row_pass = $row['password'];
+        if (password_verify($password, $row_pass)) {
+            // $_SESSION['email'] = $email;
+            $_SESSION['user_login'] = $row['user_id'];
+            $_SESSION['username'] = $row['username'];
+            $verified_status = $row['email_verified_status'];
+            if ($verified_status == '1') {
+                unset($_SESSION['login_status']);
+                // echo "ยันยัน Mail แล้ว";
                 //   $_SESSION['email'] = $email;
                 //   $_SESSION['password'] = $password;
                 //     header('location: home.php');
-                }else{
-                    // $info = "It's look like you haven't still verify your email - $email";
-                    // echo "It's look like you haven't still verify your email - $email";
-                    // echo "ดูเหมือนว่าคุณยังไม่ได้ยืนยันอีเมลของคุณ - $email";
-                    // $_SESSION['info'] = $info;
-                    // header('location: user-otp.php');
-                    $_SESSION['otp'] = $email;
-                    $_SESSION['email_new_otp'] = $email;
-                    echoJson_status_msg("warning","Looks like you haven't verified your email yet. <br>
+            } else {
+                // $info = "It's look like you haven't still verify your email - $email";
+                // echo "It's look like you haven't still verify your email - $email";
+                // echo "ดูเหมือนว่าคุณยังไม่ได้ยืนยันอีเมลของคุณ - $email";
+                // $_SESSION['info'] = $info;
+                // header('location: user-otp.php');
+                // $_SESSION['otp_status'] = $email;
+                $_SESSION['login_status'] = "email_verified_fail";
+                $_SESSION['email_new_otp'] = $email;
+                echoJson_status_msg("warning", "Looks like you haven't verified your email yet. <br>
                     Email: $email<br>
                     Click OK to proceed and complete
                     ");
-                }
-            }else{
-                echoJson_status_msg("error","Incorrect email or password!");
-                // echo "Incorrect email or password!";
             }
-        }else{
-            echoJson_status_msg("error","It's look like you're not yet a member! Click on the bottom link to signup.");
-            // echo "It's look like you're not yet a member! Click on the bottom link to signup.";
+        } else {
+            echoJson_status_msg("error", "Incorrect email or password!");
+            // echo "Incorrect email or password!";
         }
-    }else{
-        //มีปัญหาในการ execute
+    } else {
+        echoJson_status_msg("error", "It's look like you're not yet a member! Click on the bottom link to signup.");
+        // echo "It's look like you're not yet a member! Click on the bottom link to signup.";
     }
+} else {
+    //มีปัญหาในการ execute
+}
     
     // $check_email = "SELECT * FROM usertable WHERE email = '$email'";
     // $res = mysqli_query($con, $check_email);
-    
-?>
