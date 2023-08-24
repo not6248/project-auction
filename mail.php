8c8b49851158b9e72588659ea7a64c28f048c28c
not6248 <96365700+not6248@users.noreply.github.com>
2023-08-24 15:34:03 +0700

    ลบไฟล์ไม่จำเป็น

--- mail.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (mail('not-6248@hotmail.com', 'ทดสอบนะครับ', 'Mail Sender ของ Laragon ใช้ง่ายมาก', 'From: ข้อความระบบ <com40190boy@gmail.com>')) {
-    echo 'ส่งสำเร็จ';
-} else {
-    echo 'เน่า';
-}
