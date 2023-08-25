bd92eb024c31f91d5ecc897a9e5899c270e8a917
not6248 <96365700+not6248@users.noreply.github.com>
2023-08-25 23:37:40 +0700

    โค้ดทดสอบ

---
+++ page/profile/order_bidder/index.php
@@ -0,0 +1,13 @@
+<div class="card" style="background: rgb(236,238,249);box-shadow: 0px 4px 4px rgba(33,37,41,0.25);">
+    <div class="card-body">
+        <h4 class="card-title mb-4" style="font-size: 28px;">Profile Detail</h4>
+        <div class="card">
+            <div class="card-header" style="background: #D8DBE9;">
+                <h4 style="color: rgb(84,88,94);" class="mb-0">Profile Editor</h4>
+            </div>
+            <div class="card-body" style="background: #ECEEF9;">
+            </div>
+            <div class="card-footer" style="background: #D8DBE9;"><button class="btn btn-primary" type="button" style="border-radius: 7px;">ยืนยันการแก้ไข</button></div>
+        </div>
+    </div>
+</div>
