a2550090ece5d476326010d9483ecb43b4ea88f6
not6248 <96365700+not6248@users.noreply.github.com>
2023-08-27 17:49:50 +0700

    เพิ่ม sweetalert.js

--- includes/scripts.php
+++ includes/scripts.php
@@ -2,3 +2,5 @@
     <script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- sweetalert2 -->
     <script src="assets/js/sweetalert2.all.min.js"></script>
+<!-- โค้ดแจ้งเตือนของ sweetalert2-->
+    <script src="./assets/js/sweetalert.js"></script>
