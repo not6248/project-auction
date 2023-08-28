4669be6bd43bb580342171a9a5b80b69ed2ee1f7
not6248 <96365700+not6248@users.noreply.github.com>
2023-08-28 23:29:58 +0700

    ทดสอบหน้า order

--- page/profile/order_bidder/index.php
+++ page/profile/order_bidder/index.php
@@ -1,13 +1,255 @@
 <div class="card" style="background: rgb(236,238,249);box-shadow: 0px 4px 4px rgba(33,37,41,0.25);">
     <div class="card-body">
-        <h4 class="card-title mb-4" style="font-size: 28px;">Profile Detail</h4>
-        <div class="card">
-            <div class="card-header" style="background: #D8DBE9;">
-                <h4 style="color: rgb(84,88,94);" class="mb-0">Profile Editor</h4>
+        <div class="d-flex justify-content-between">
+            <h4 class="card-title mb-6" style="font-size: 28px;">Order Bidder</h4>
+            <button type="button" class="btn btn-primary btn-sm">Small button</button>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header" style="background: #D8DBE9;">
+                        <h4 style="color: rgb(84,88,94);" class="mb-0">Order</h4>
+                    </div>
+                    <div class="card-body" style="background: #ECEEF9;">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>1</th>
+                                    <th>2</th>
+                                    <th>3</th>
+                                    <th>4</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>d</td>
+                                    <td>d</td>
+                                    <td>d</td>
+                                    <td>d</td>
+                                </tr>
+                                <tr>
+                                    <td>d</td>
+                                    <td>dd</td>
+                                    <td>d</td>
+                                    <td>d</td>
+                                </tr>
+                                <tr>
+                                    <td>d</td>
+                                    <td>d</td>
+                                    <td>d</td>
+                                    <td>d</td>
+                                </tr>
+                                <tr>
+                                    <td>d</td>
+                                    <td>d</td>
+                                    <td>d</td>
+                                    <td>d</td>
+                                </tr>
+                                <tr>
+                                    <td>d</td>
+                                    <td>d</td>
+                                    <td>d</td>
+                                    <td>d</td>
+                                </tr>
+                                <tr>
+                                    <td>d</td>
+                                    <td>dd</td>
+                                    <td>d</td>
+                                    <td>d</td>
+                                </tr>
+                                <tr>
+                                    <td>d</td>
+                                    <td>d</td>
+                                    <td>d</td>
+                                    <td>d</td>
+                                </tr>
+                                <tr>
+                                    <td>d</td>
+                                    <td>d</td>
+                                    <td>d</td>
+                                    <td>d</td>
+                                </tr>
+                                <tr>
+                                    <td>d</td>
+                                    <td>dd</td>
+                                    <td>d</td>
+                                    <td>d</td>
+                                </tr>
+                                <tr>
+                                    <td>d</td>
+                                    <td>d</td>
+                                    <td>d</td>
+                                    <td>d</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
-            <div class="card-body" style="background: #ECEEF9;">
+        </div>
+        <div class="row mt-4">
+            <div class="col-6">
+                <div class="card">
+                    <div class="card-header" style="background: #D8DBE9;">
+                        <h4 style="color: rgb(84,88,94);" class="mb-0">Order</h4>
+                    </div>
+                    <div class="card-body" style="background: #ECEEF9;">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>1</th>
+                                    <th>2</th>
+                                    <th>3</th>
+                                    <th>4</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>d</td>
+                                    <td>d</td>
+                                    <td>d</td>
+                                    <td>d</td>
+                                </tr>
+                                <tr>
+                                    <td>d</td>
+                                    <td>dd</td>
+                                    <td>d</td>
+                                    <td>d</td>
+                                </tr>
+                                <tr>
+                                    <td>d</td>
+                                    <td>d</td>
+                                    <td>d</td>
+                                    <td>d</td>
+                                </tr>
+                                <tr>
+                                    <td>d</td>
+                                    <td>d</td>
+                                    <td>d</td>
+                                    <td>d</td>
+                                </tr>
+                                <tr>
+                                    <td>d</td>
+                                    <td>d</td>
+                                    <td>d</td>
+                                    <td>d</td>
+                                </tr>
+                                <tr>
+                                    <td>d</td>
+                                    <td>dd</td>
+                                    <td>d</td>
+                                    <td>d</td>
+                                </tr>
+                                <tr>
+                                    <td>d</td>
+                                    <td>d</td>
+                                    <td>d</td>
+                                    <td>d</td>
+                                </tr>
+                                <tr>
+                                    <td>d</td>
+                                    <td>d</td>
+                                    <td>d</td>
+                                    <td>d</td>
+                                </tr>
+                                <tr>
+                                    <td>d</td>
+                                    <td>dd</td>
+                                    <td>d</td>
+                                    <td>d</td>
+                                </tr>
+                                <tr>
+                                    <td>d</td>
+                                    <td>d</td>
+                                    <td>d</td>
+                                    <td>d</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
-            <div class="card-footer" style="background: #D8DBE9;"><button class="btn btn-primary" type="button" style="border-radius: 7px;">ยืนยันการแก้ไข</button></div>
+            <div class="col-6">
+                <div class="card">
+                    <div class="card-header" style="background: #D8DBE9;">
+                        <h4 style="color: rgb(84,88,94);" class="mb-0">Order</h4>
+                    </div>
+                    <div class="card-body" style="background: #ECEEF9;">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>1</th>
+                                    <th>2</th>
+                                    <th>3</th>
+                                    <th>4</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>d</td>
+                                    <td>d</td>
+                                    <td>d</td>
+                                    <td>d</td>
+                                </tr>
+                                <tr>
+                                    <td>d</td>
+                                    <td>dd</td>
+                                    <td>d</td>
+                                    <td>d</td>
+                                </tr>
+                                <tr>
+                                    <td>d</td>
+                                    <td>d</td>
+                                    <td>d</td>
+                                    <td>d</td>
+                                </tr>
+                                <tr>
+                                    <td>d</td>
+                                    <td>d</td>
+                                    <td>d</td>
+                                    <td>d</td>
+                                </tr>
+                                <tr>
+                                    <td>d</td>
+                                    <td>d</td>
+                                    <td>d</td>
+                                    <td>d</td>
+                                </tr>
+                                <tr>
+                                    <td>d</td>
+                                    <td>dd</td>
+                                    <td>d</td>
+                                    <td>d</td>
+                                </tr>
+                                <tr>
+                                    <td>d</td>
+                                    <td>d</td>
+                                    <td>d</td>
+                                    <td>d</td>
+                                </tr>
+                                <tr>
+                                    <td>d</td>
+                                    <td>d</td>
+                                    <td>d</td>
+                                    <td>d</td>
+                                </tr>
+                                <tr>
+                                    <td>d</td>
+                                    <td>dd</td>
+                                    <td>d</td>
+                                    <td>d</td>
+                                </tr>
+                                <tr>
+                                    <td>d</td>
+                                    <td>d</td>
+                                    <td>d</td>
+                                    <td>d</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
