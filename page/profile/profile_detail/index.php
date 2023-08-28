0d8781cdb89a040f95c94f44aa6022b9295aae53
not6248 <96365700+not6248@users.noreply.github.com>
2023-08-28 23:30:19 +0700

    แก้ไขการส่งข้อมูล

--- page/profile/profile_detail/index.php
+++ page/profile/profile_detail/index.php
@@ -2,6 +2,7 @@
 $sql = "SELECT * FROM `login` INNER JOIN `user_detail` USING(user_id) WHERE user_id = " . $_SESSION['user_login'];
 $result = mysqli_query($conn, $sql);
 $row = mysqli_fetch_assoc($result);
+$prefix = $row['prefix_id'];
 ?>
 
 <div class="card" style="background: rgb(236,238,249);box-shadow: 0px 4px 4px rgba(33,37,41,0.25);">
@@ -12,8 +13,8 @@ $row = mysqli_fetch_assoc($result);
                 <h4 style="color: rgb(84,88,94);" class="mb-0">Profile Editor</h4>
             </div>
             <div class="card-body" style="background: #ECEEF9;">
-            <!-- form -->
-                <form action="./page/profile/profile_detail/profile_detail_update.php" method="post">
+                <!-- form -->
+                <form id="profil-detail-update-form" action="./ajax/ajax_profile_detail_update.php" method="post">
                     <div class="mb-1">
                         <p class="mb-0 ms-1">ชื่อ</p>
                         <input required type="text" name="fname" value="<?= $row['ud_fname'] ?>" style="border-radius: 5px;border: 1px solid #CCCCCC;height: 35px;width: 100%;" class="ms-0 ps-3">
@@ -32,22 +33,29 @@ $row = mysqli_fetch_assoc($result);
                     </div>
                     <div class="mb-1">
                         <p class="mb-0 ms-1">เบอร์โทรศัพท์</p>
-                        <input required type="text" name="tele" value="<?= $row['ud_phone'] ?>" style="border-radius: 5px;border: 1px solid #CCCCCC;height: 35px;width: 100%;" class="ms-0 ps-3">
+                        <input required  type="number" name="tele" value="<?= $row['ud_phone'] ?>" style="border-radius: 5px;border: 1px solid #CCCCCC;height: 35px;width: 100%;" class="ms-0 ps-3">
                     </div>
+                    <!-- radio -->
+                    <!-- sql radio -->
+                    <?php 
+                    $sql = "SELECT * FROM `prefix`";
+                    $resultprefix = mysqli_query($conn,$sql);
+
+                    ?>
                     <div class="mb-1">
                         <p class="mb-0 ms-1">คำนำหน้าชื่อ</p>
                         <div class="d-flex ps-3">
                             <div class="me-4">
                                 <!-- checked="checked" -->
-                                <input class="form-check-input" type="radio" id="prefix-1" name="prefix">
-                                <label class="form-check-label me-2" for="prefix-1">นาย</label>
-                                <input class="form-check-input" type="radio" id="prefix-2" name="prefix">
-                                <label class="form-check-label me-2" for="prefix-2">นาง</label>
-                                <input class="form-check-input" type="radio" id="prefix-3" name="prefix">
-                                <label class="form-check-label" for="prefix-3">นางสาว</label>
+                                <?php foreach($resultprefix as $row) :?>
+                                <input required value="<?=$row['prefix_id']?>" type="radio" class="form-check-input" name="prefix"
+                                <?=isset($prefix) && $prefix == $row['prefix_id'] ? "checked" : ""?>>
+                                <label class="form-check-label me-2" for="prefix-1"><?=$row['prefix_name']?></label>
+                                <?php endforeach ?>
                             </div>
                         </div>
                     </div>
+                    <!-- End radio -->
                     <div class="mb-5">
                         <p class="mb-0 ms-1">รหัสบัตรประชาชน</p>
                         <input disabled type="text" value="" style="border-radius: 5px;border: 1px solid #CCCCCC;height: 35px;width: 100%;" class="ms-0 ps-3">
