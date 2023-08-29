ab1f5485df2de4ffae6f5198dcbb6d9b0c785285
not6248 <96365700+not6248@users.noreply.github.com>
2023-08-30 03:30:03 +0700

    ปิดการใช้งาน js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,28 @@
-$(document).ready(function () {
-    loadProducts(1);
+// $(document).ready(function () {
+//     loadProducts(1);
 
-    $(".category-btn").click(function () {
-        $(".category-btn").removeClass("btn-primary").addClass("btn-light");
-        $(this).removeClass("btn-light").addClass("btn-primary");
-        let pro_typeID = $(this).data("pro-type");
-        console.log(pro_typeID);
-        loadProducts(pro_typeID);
-    });
+//     $(".category-btn").click(function () {
+//         $(".category-btn").removeClass("btn-primary").addClass("btn-light");
+//         $(this).removeClass("btn-light").addClass("btn-primary");
+//         let pro_typeID = $(this).data("pro-type");
+//         console.log(pro_typeID);
+//         loadProducts(pro_typeID);
+//     });
 
-    function loadProducts(pro_typeID) {
-        $.ajax({
-            url: "./ajax/ajax_product_list.php", // Replace with your server-side script to fetch products
-            method: "GET",
-            data: { pd_type_id: pro_typeID },
-            success: function (data) {
-                $("#product-list").html(data);
-            },
-            error: function (error) {
-                console.error("Error loading products:", error);
-            }
-        });
-    }
-});
+//     function loadProducts(pro_typeID) {
+//         $.ajax({
+//             url: "./ajax/ajax_product_list.php", // Replace with your server-side script to fetch products
+//             method: "GET",
+//             data: { pd_type_id: pro_typeID },
+//             success: function (data) {
+//                 $("#product-list").html(data);
+//             },
+//             error: function (error) {
+//                 console.error("Error loading products:", error);
+//             }
+//         });
+//     }
+// });
 
 
 
