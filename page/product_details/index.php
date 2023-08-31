fe7f0a2f0ea707c49f48e0b5af57a11a8c5ae0b4
not6248 <96365700+not6248@users.noreply.github.com>
2023-09-01 00:31:16 +0700

    เปลี่ยนชื่อโฟลเดอร์

---
+++ page/product_details/index.php
@@ -0,0 +1,119 @@
+<div class="container mt-xl-5 pt-xl-0">
+    <div></div>
+    <div class="row justify-content-center">
+        <div class="col-sm-10 col-md-6 col-lg-4 col-xl-4 mt-4 mt-md-0"><img width="423" height="352" src="assets/img/717QYH6fZsL%201.png" style="width: 100%;height: 300px;"></div>
+        <div class="col-sm-10 col-md-5 d-lg-none pt-0 mt-4 mb-4 mt-md-0 ms-sm-0 ps-sm-0 pe-sm-0" style="text-align: left;">
+            <div class="d-flex flex-column justify-content-center align-items-center mb-0" style="height: 190px;border-radius: 16px;border: 3px solid rgb(13,110,253);">
+                <div class="row" style="width: 100%;">
+                    <div class="col-4">
+                        <span>เหลือเวลา<br>1 วัน 5 ชม.</span>
+                    </div>
+                    <div class="col-4 d-flex justify-content-center align-items-end pe-0 ps-0" style="text-align: center;">
+                        <span class="fw-bold" style="color: rgb(62, 0, 186);">ราคาปัจจุบัน!</span>
+                    </div>
+                    <div class="col-4 ms-0" style="text-align: right;">
+                        <i style="font-size: 20px;" class="bi bi-star-fill"></i>
+                    </div>
+                </div>
+                <div class="mb-3" style="min-width: 90%;text-align: center;border-bottom: 1px solid rgba(33,37,41,0.3);">
+                    <span style="font-size: 23px;font-weight: bold;color: #3E168E;">15,000฿</span>
+                </div>
+                <div class="mb-1">
+                    <button class="btn btn-primary" type="button" style="font-size: 20px;width: 263.906px;">เสนอราคา</button>
+                </div>
+                <div class="">
+                    <span class="fw-bold" style="color: rgb(62, 0, 186);">จำนวนผู้ประมูล 7 คน</span>
+                </div>
+            </div>
+        </div>
+        <div class="col-11 col-lg-4 col-xl-4 col-xxl-4 mt-0">
+            <div>
+                <h1 style="font-size: 16px;font-weight: bold;" class="mb-4">The White Album - 50th Anniversary 4LP
+                    <br>Deluxe Edition + Booklet / Fold-out Poster
+                </h1>
+                <div class="row pt-xl-2 pb-xl-2 ms-xl-0 me-xl-0" style="border-top: 1px solid rgba(33,37,41,0.3) ;border-bottom: 1px none rgba(33,37,41,0.3) ;">
+                    <div class="col" style="font-weight: bold;"><span>จำนวน</span></div>
+                    <div class="col-md-6 col-lg-5 col-xl-8 col-xxl-7 ps-xl-0"><span style="margin-left: 151px;" class="ms-xl-0 ms-0">5ชิ้น</span></div>
+                </div>
+                <div class="row pt-xl-2 pb-xl-2 ms-xl-0 me-xl-0" style="border-top: 1px solid rgba(33,37,41,0.3) ;border-bottom-width: 1px;border-bottom-style: none;">
+                    <div class="col"><span style="font-weight: bold;">ราคาเริ่มต้น</span></div>
+                    <div class="col-md-6 col-lg-5 col-xl-8 col-xxl-7 ps-xl-0"><span style="margin-left: 151px;" class="ms-xl-0 ps-0 ms-0">1,000 บาท</span></div>
+                </div>
+                <div class="row pt-xl-2 pb-xl-2 ms-xl-0 me-xl-0" style="border-top: 1px solid rgba(33,37,41,0.3) ;border-bottom-width: 1px;border-bottom-style: none;">
+                    <div class="col"><span style="font-weight: bold;">ราคาปัจจุบัน</span></div>
+                    <div class="col-md-6 col-lg-5 col-xl-8 col-xxl-7 ps-xl-0"><span style="margin-left: 151px;" class="ms-xl-0 ps-0 ms-0">15,000 บาท</span></div>
+                </div>
+                <div class="row pt-xl-2 pb-xl-2 ms-xl-0 me-xl-0" style="border-top: 1px solid rgba(33,37,41,0.3) ;border-bottom-width: 1px;border-bottom-style: none;">
+                    <div class="col"><span style="font-weight: bold;">สถานะประมูล</span></div>
+                    <div class="col-md-6 col-lg-5 col-xl-8 col-xxl-7 ps-xl-0"><span style="margin-left: 151px;" class="ms-xl-0 ms-0">กำลังประมูล</span></div>
+                </div>
+                <div class="row pt-xl-2 pb-xl-2 ms-xl-0 me-xl-0" style="border-top: 1px solid rgba(33,37,41,0.3) ;border-bottom-width: 1px;border-bottom-style: none;">
+                    <div class="col"><span style="font-weight: bold;">เหลือเวลา</span></div>
+                    <div class="col-md-6 col-lg-5 col-xl-8 col-xxl-7 ps-xl-0"><span style="margin-left: 151px;" class="ms-xl-0 ps-0 ms-0">5 ชม. 30 นาที | วันจันทร์, 16:30</span></div>
+                </div>
+                <div class="row pt-xl-2 pb-xl-2 ms-xl-0 me-xl-0" style="border-top: 1px solid rgba(33,37,41,0.3) ;border-bottom-width: 1px;border-bottom-style: none;">
+                    <div class="col"><span style="font-weight: bold;">รหัสการประมูล</span></div>
+                    <div class="col-md-6 col-lg-5 col-xl-8 col-xxl-7 ps-xl-0"><span style="margin-left: 151px;" class="ms-xl-0 ms-0">0000000001</span></div>
+                </div>
+                <div class="row pt-xl-2 pb-xl-2 ms-xl-0 me-xl-0" style="border-top: 1px solid rgba(33,37,41,0.3) ;border-bottom-width: 1px;border-bottom-style: none;">
+                    <div class="col"><span style="font-weight: bold;">สภาพสินค้า</span></div>
+                    <div class="col-md-6 col-lg-5 col-xl-8 col-xxl-7 ps-xl-0"><span style="margin-left: 151px;" class="ms-xl-0 ms-0">สภาพดีไม่มีความเสียหาย</span></div>
+                </div>
+                <div class="row pt-xl-2 pb-xl-2 ms-xl-0 me-xl-0" style="border-top: 1px solid rgba(33,37,41,0.3) ;border-bottom-width: 1px;border-bottom-style: none;">
+                    <div class="col"><span style="font-weight: bold;">ชื่อผู้ขาย</span></div>
+                    <div class="col-md-6 col-lg-5 col-xl-8 col-xxl-7 ps-xl-0"><span style="margin-left: 151px;" class="ms-xl-0 ms-0">M*n***59</span></div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-11 col-lg-4 d-none d-sm-none d-md-none d-lg-block">
+            <div class="d-flex flex-column justify-content-center align-items-center mb-0" style="height: 190px;border-radius: 16px;border: 3px solid rgb(13,110,253);">
+                <div class="row" style="width: 100%;">
+                    <div class="col-4">
+                        <span>เหลือเวลา<br>1 วัน 5 ชม.</span>
+                    </div>
+                    <div class="col-4 d-flex justify-content-center align-items-end pe-0 ps-0" style="text-align: center;">
+                        <span class="fw-bold" style="color: rgb(62, 0, 186);">ราคาปัจจุบัน!</span>
+                    </div>
+                    <div class="col-4 ms-0" style="text-align: right;">
+                        <i style="font-size: 20px;" class="bi bi-star-fill"></i>
+                    </div>
+                </div>
+                <div class="mb-3" style="min-width: 90%;text-align: center;border-bottom: 1px solid rgba(33,37,41,0.3);">
+                    <span style="font-size: 23px;font-weight: bold;color: #3E168E;">15,000฿</span>
+                </div>
+                <div class="mb-1">
+                    <button class="btn btn-primary" type="button" style="font-size: 20px;width: 263.906px;">เสนอราคา</button>
+                </div>
+                <div class="">
+                    <span class="fw-bold" style="color: rgb(62, 0, 186);">จำนวนผู้ประมูล 7 คน</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row pt-lg-0 mt-lg-4" style="padding-top: 0px;">
+        <div class="col-lg-4 col-xl-4 d-none d-lg-block"></div>
+        <div class="col-12 col-sm-9 col-lg-7 col-xl-7 col-xxl-7 ps-md-4 ps-4 ps-lg-3 pt-0 mt-4 mt-lg-0">
+            <div style="border-bottom-style: solid;border-bottom-color: #3E168E;">
+                <div style="background: #3E168E;border-radius: 0;border-top-left-radius: 6px;border-top-right-radius: 6px;width: 186px;" class="ps-xl-2 pe-xl-0 mt-0 pt-1"><span style="color: rgb(255,255,255);" class="ps-3 ms-0 ms-xl-0 ps-xl-2 mt-xl-0 pt-0">คำอธิบายเกี่ยวกับสินค้า</span></div>
+            </div><span><br><span style="color: rgb(0, 0, 0);">Lorem ipsum dolor sit amet, consectetur adipiscing
+                    elit. Etiam feugiat ante at arcu ultrices, sed convallis nibh volutpat. Sed porttitor varius
+                    cursus. Maecenas a erat congue mauris scelerisque iaculis. Quisque eu sem vulputate ante semper
+                    sagittis. Praesent sollicitudin leo dictum nunc efficitur gravida sed nec erat. Sed ex augue,
+                    volutpat nec neque ut, fermentum vestibulum purus. Aenean quam massa, placerat eget ornare eget,
+                    vulputate at sapien. Fusce volutpat massa imperdiet, accumsan leo nec, fermentum felis.Donec
+                    tincidunt luctus velit, id fringilla libero commodo sit amet. Class aptent taciti sociosqu ad
+                    litora torquent per conubia nostra, per inceptos himenaeos. Curabitur quis posuere orci. In
+                    ultricies condimentum molestie. Proin erat dui, scelerisque vel libero eu, sodales ultrices est.
+                    Sed aliquet ex massa, mattis lacinia tellus placerat nec. Fusce nibh metus, malesuada sit amet
+                    turpis nec, eleifend facilisis dolor. Maecenas nec ultricies leo. Nullam sit amet augue
+                    dignissim, porttitor massa eu, suscipit nisi. Quisque sed tristique felis. Vestibulum ante ipsum
+                    primis in faucibus orci luctus et ultrices posuere cubilia curae; Praesent et purus sed ante
+                    vestibulum semper a a nisl. Nam imperdiet ante quis bibendum viverra. Cras et tortor sit amet
+                    leo molestie fermentum. Mauris leo enim, condimentum sit amet posuere nec, cursus ornare
+                    massa.Vivamus tempus purus nisi, ac tristique elit mollis ac. Morbi dapibus enim tincidunt velit
+                    interdum porta. Duis et scelerisque lacus. Sed varius venenatis quam, at accumsan justo blandit
+                    in. Nulla in lorem dictum, dapibus risus eget, eleifend lectus. Nunc sapien nulla, commodo a
+                    dignissim in, imperdiet id risus. Etiam nec hendrerit dui.</span><br><br></span>
+        </div>
+    </div>
+</div>
