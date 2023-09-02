c2aee6f03fc7a847fb6417f0d2134dcac3449c55
not6248 <96365700+not6248@users.noreply.github.com>
2023-09-02 21:54:15 +0700

    เพิ่มเงื่อนไขสำหรับหน้าproduct,order_seller
    และแก้ไขเงื่อนเดิมจาก if else เป็น สวิท

--- page/profile/index.php
+++ page/profile/index.php
@@ -1,25 +1,50 @@
-<?php 
+<?php
 if (!isset($_SESSION['user_login']) && empty($_SESSION['user_login'])) {
     echo '<script>window.location.href = "./";</script>';
     exit; // ออกจากการทำงานของสคริปต์ PHP เพื่อป้องกันการแสดงผลเนื้อหาหลังจากนี้
-}?>
+} elseif ((isset($_GET['subpage']) && in_array($_GET['subpage'], ['product', 'order_seller']))) {
+    //เช็ค page ที่จำเป็นต้องเช็ค user_type
+    if (isset($_SESSION['user_type']) && $_SESSION['user_type'] != 2) {
+        echo '<script>window.location.href = "./?page=profile";</script>';
+        exit; // ออกจากการทำงานของสคริปต์ PHP เพื่อป้องกันการแสดงผลเนื้อหาหลังจากนี้
+    }
+} ?>
+
+<!-- product -->
+<!-- order_seller -->
 <div class="container py-5">
     <div class="row mt-5">
         <div class="col">
-            <?php include 'menu.php'?>
+            <?php include 'menu.php' ?>
         </div>
         <div style="min-height: 80vh;" class="col-sm-auto col-md-8 col-lg-8 col-xl-9 col-xxl-9">
             <?php
-            if (!isset($_GET['subpage']) && empty($_GET['subpage'])) {
-                include 'profile_detail/index.php';
-            } elseif ((isset($_GET['subpage']) && $_GET['subpage'] == 'order_bidder')) {
-                include 'order_bidder/index.php';
-            } elseif ((isset($_GET['subpage']) && $_GET['subpage'] == 'favorite')) {
-                echo "favorite";
-            } elseif ((isset($_GET['subpage']) && $_GET['subpage'] == 'product')) {
-                include 'product/index.php';
-            } elseif ((isset($_GET['subpage']) && $_GET['subpage'] == 'order_seller')) {
-                include 'order_seller/index.php';
+            $subpage = isset($_GET['subpage']) ? $_GET['subpage'] : '';
+            $function = isset($_GET['function']) ? $_GET['function'] : '';
+
+            switch ($subpage) {
+                case '':
+                    include 'profile_detail/index.php';
+                    break;
+                case 'order_bidder':
+                    include 'order_bidder/index.php';
+                    break;
+                case 'favorite':
+                    echo "favorite";
+                    break;
+                case 'product':
+                    if ($function == 'add') {
+                        include 'product/insert.php';
+                    } else {
+                        include 'product/index.php';
+                    }
+                    break;
+                case 'order_seller':
+                    include 'order_seller/index.php';
+                    break;
+                default:
+                    include 'profile_detail/index.php';
+                    break;
             }
             ?>
         </div>
