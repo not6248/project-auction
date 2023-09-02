78cfbbbd9249ac0dfb63da162b930bfd93249f66
not6248 <96365700+not6248@users.noreply.github.com>
2023-09-02 21:53:44 +0700

    ลบเงื่อนไขเดิมออก

--- page/profile/order_seller/index.php
+++ page/profile/order_seller/index.php
@@ -1,5 +1,1 @@
-<?php 
-if (isset($_SESSION['user_type']) && $_SESSION['user_type'] != 2) {
-    echo '<script>window.location.href = "./?page=profile";</script>';
-    exit; // ออกจากการทำงานของสคริปต์ PHP เพื่อป้องกันการแสดงผลเนื้อหาหลังจากนี้
-}?>
+<?php "order_seller"?>
