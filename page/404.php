0781173089e016dc6ca6ad3b59f79fc67fb549d1
not6248 <96365700+not6248@users.noreply.github.com>
2023-09-05 01:55:38 +0700

    สร้างหน้า 404

---
+++ page/404.php
@@ -0,0 +1,24 @@
+<main class="form-signin w-100 m-auto mt-1">
+  <div class="container">
+
+    <!-- Main content -->
+    <section class="content mt-5">
+      <div class="error-page">
+        <h1 class="headline text-warning display-1"> 404</h1>
+
+        <div class="error-content">
+          <h3><i class="fas fa-exclamation-triangle text-warning"></i> Oops! Page not found.</h3>
+
+          <p>
+            We could not find the page you were looking for.
+            Meanwhile, you may <a href="./">return to home page</a> or try using the search form.
+          </p>
+
+        </div>
+
+      </div>
+
+    </section>
+  </div>
+
+</main>
