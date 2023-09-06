ca66f0223691ad11200178ab0df9874ed7359447
not6248 <96365700+not6248@users.noreply.github.com>
2023-09-06 23:51:11 +0700

    Create variable.php

---
+++ db/variable.php
@@ -0,0 +1,14 @@
+<?php 
+$pd_condition_array = array("ไม่เคยใช้", "เหมือนใหม่", "มีตำหนิเล็กน้อย", "เก่า", "เสียหาย");
+// <div class="form-group mb-2">
+// <label>สภาพของสินค้า</label>
+// <select name="pd_condition" class="form-control" required="required">
+//     <option value="" selected disabled>สภาพสินค้า</option>
+//     <option value="0">ไม่เคยใช้</option>
+//     <option value="1">เหมือนใหม่</option>
+//     <option value="2">มีตำหนิเล็กน้อย</option>
+//     <option value="3">เก่า</option>
+//     <option value="4">เสียหาย</option>
+// </select>
+// </div>
+?>
