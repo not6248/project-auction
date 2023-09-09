c3b98ce1a530a0cd262ea4b2c452dc2b01ec88ef
not6248 <96365700+not6248@users.noreply.github.com>
2023-09-09 19:48:48 +0700

    ใช้ key จาก pd_condition_arr เป็น value

--- page/profile/product/insert.php
+++ page/profile/product/insert.php
@@ -64,9 +64,9 @@
                                     <label>สภาพของสินค้า</label>
                                     <select name="pd_condition" class="form-control" required="required">
                                         <option value="" selected disabled>สภาพสินค้า</option>
-                                        <?php $i = -1;
-                                        foreach($pd_condition_array as $arr_condition) :?>
-                                        <option value="<?=$i++?>"><?=$arr_condition[$i++]?></option>
+                                        <?php ;
+                                        foreach($pd_condition_arr as $index => $v) :?>
+                                        <option value="<?=$index?>"><?=$v?></option>
                                         <?php endforeach?>
                                         <!-- <option value="1">เหมือนใหม่</option>
                                         <option value="2">มีตำหนิเล็กน้อย</option>
