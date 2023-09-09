9971c53df50031939b477a80b24b15b6efc5a6c8
not6248 <96365700+not6248@users.noreply.github.com>
2023-09-09 19:44:49 +0700

    สร้างฟังชั่น os_sts_user

--- db/function.php
+++ db/function.php
@@ -5,4 +5,12 @@ function echoJson_status_msg($status, $msg)
     echo json_encode(array("status" => $status, "msg" => $msg));
     exit();
 }
+
+function os_sts_user($os_id,$os_name_arr){
+    if(!($os_id > 0)){
+        return $os_name_arr[0];
+    }else{
+        return $os_name_arr[1];
+    }
+}
 ?>
