e5593aeef535f4bcf9bd12d8ccc3d1191ccfea25
not6248 <96365700+not6248@users.noreply.github.com>
2023-09-10 06:16:27 +0700

    Update ไปใช้ stmt เพื่อความปลอดภัย

--- ajax/ajax_profile_detail_update.php
+++ ajax/ajax_profile_detail_update.php
@@ -2,25 +2,28 @@
 session_start();
 include '../db/db_conn.php';
 
+if (isset($_POST) && !empty(isset($_POST)) && !empty($_SESSION['user_login'])) {
+    $prefix_id  = $_POST["prefix"];
+    $ud_fname   = $_POST["fname"];
+    $ud_lname   = $_POST["lname"];
+    $ud_address = $_POST["address"];
+    $ud_phone   = $_POST["tele"];
+    $user_id    = $_SESSION['user_login'];
 
-$ud_fname = $_POST["fname"];
-$ud_lname = $_POST["lname"];
-$ud_address = $_POST["address"];
-$ud_phone = $_POST["tele"];
-$prefix_id = $_POST["prefix"];
+    $sql = "UPDATE `user_detail` SET 
+    prefix_id   = ?, 
+    ud_fname    = ?, 
+    ud_lname    = ?, 
+    ud_address  = ?, 
+    ud_phone    = ? 
+    WHERE user_id = ?";
 
-$sql = "UPDATE `user_detail` SET 
-prefix_id = $prefix_id, 
-ud_fname ='$ud_fname', 
-ud_lname = '$ud_lname', 
-ud_address = '$ud_address', 
-ud_phone = '$ud_phone'
-WHERE user_id = " . $_SESSION['user_login'];
+    $stmt = mysqli_prepare($conn,$sql);
+    mysqli_stmt_bind_param($stmt,"issssi",$prefix_id,$ud_fname,$ud_lname,$ud_address,$ud_phone,$user_id);
 
-$result = mysqli_query($conn, $sql);
-
-if($result){
-    echo 'success';
-}else{
-    echo 'error';
+    if (mysqli_stmt_execute($stmt)) {
+        echo 'success';
+    } else {
+        echo 'error';
+    }
 }
