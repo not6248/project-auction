68ff541054e1694bfc5e777c7fa854aa24f11127
not6248 <96365700+not6248@users.noreply.github.com>
2023-09-10 06:11:37 +0700

    นำ DataTable ไปทำ fn data_tb_th
    นำ DataTable ไปทำ fn data_tb_th
    และทำปุ่มลบ โยงไปที่ ajax_product_delete.php

--- page/profile/product/index.php
+++ page/profile/product/index.php
@@ -45,7 +45,7 @@
                                             <td class="">
                                                 <a href="" class="btn btn-info btn-sm">รายละเอียด</a>
                                                 <a href="./?page=<?=$_GET['page']?>&subpage=<?=$_GET['subpage']?>&function=edit" class="btn btn-warning btn-sm">แก้ไข</a>
-                                                <a href="./?page=<?=$_GET['page']?>&subpage=<?=$_GET['subpage']?>&function=delete" class="btn btn-danger btn-sm" onclick="return confirm('คุณต้องการลบข้อมูลของ admin หรือไม่')">ลบ</a>
+                                                <a href="./ajax/ajax_product_delete.php" data-pd-id="<?=$row['pd_id']?>" class="btn btn-danger btn-sm product-del-btn">ลบ</a>
                                             </td>
                                         </tr>
                                     <?php endforeach ?>
@@ -64,33 +64,7 @@
 </div>
 
 <script>
-    $(document).ready(function() {
-        $('#seller_product_list').DataTable({
-            responsive: true,
-            language: {
-                "decimal": "",
-                "emptyTable": "ไม่มีข้อมูลในตาราง",
-                "info": "กำลังแสดง _START_ ถึง _END_ จาก _TOTAL_ รายการ",
-                "infoEmpty": "กำลังแสดง 0 ถึง 0 จาก 0 รายการ",
-                "infoFiltered": "<br>(กรองจากทั้งหมด _MAX_ รายการ)",
-                "infoPostFix": "",
-                "thousands": ",",
-                "lengthMenu": "แสดง _MENU_ รายการ",
-                "loadingRecords": "กำลังโหลด...",
-                "processing": "",
-                "search": "ค้นหา:",
-                "zeroRecords": "ไม่พบบันทึกที่ตรงกัน",
-                "paginate": {
-                    "first": "อันดับแรก",
-                    "last": "ล่าสุด",
-                    "next": "ต่อไป",
-                    "previous": "ก่อนหน้า"
-                },
-                "aria": {
-                    "sortAscending": ": เปิดใช้งานเพื่อจัดเรียงคอลัมน์จากน้อยไปมาก",
-                    "sortDescending": ": เปิดใช้งานเพื่อจัดเรียงคอลัมน์จากมากไปน้อย"
-                }
-            }
-        });
-    });
+    data_tb_th("#seller_product_list");
+
+
 </script>
