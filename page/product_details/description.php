5c58295decea44b1f61ebdcc26502bea797c8d8e
not6248 <96365700+not6248@users.noreply.github.com>
2023-09-11 20:51:25 +0700

    Create description.php

---
+++ page/product_details/description.php
@@ -0,0 +1,10 @@
+<div class="col-12 col-sm-9 col-lg-7 col-xl-7 col-xxl-7 ps-md-4 ps-4 ps-lg-3 pt-0 mt-4 mt-lg-0 offset-lg-4">
+    <div style="border-bottom-style: solid;border-bottom-color: #3E168E;">
+        <div style="background: #3E168E;border-radius: 0;border-top-left-radius: 6px;border-top-right-radius: 6px;width: 186px;" class="ps-xl-2 pe-xl-0 mt-0 pt-1"><span style="color: rgb(255,255,255);" class="ps-3 ms-0 ms-xl-0 ps-xl-2 mt-xl-0 pt-0">คำอธิบายเกี่ยวกับสินค้า</span></div>
+    </div>
+    <div class="mt-3 mb-4">
+        <span class="mt-2" style="color: rgb(0, 0, 0);">
+            <?= $row['pd_detail'] ?>
+        </span>
+    </div>
+</div>
