3e24c9f989057eadafd85da8148306acc6155a23
not6248 <96365700+not6248@users.noreply.github.com>
2023-09-18 21:46:19 +0700

    แก้ไขหน้า index id_card_verified

--- admin_panel/page/id_card_verified/index.php
+++ admin_panel/page/id_card_verified/index.php
@@ -1,10 +1,22 @@
+<?php
+$sql1 = "SELECT * FROM user_detail WHERE ud_id_card IS NULL AND ud_idcard_img IS NOT NULL;";
+$result1 = mysqli_query($conn, $sql1);
+
+$sql2 = "SELECT * FROM user_detail WHERE ud_id_card = 0;";
+$result2 = mysqli_query($conn, $sql2);
+
+$sql3 = "SELECT ud.* FROM user_detail ud INNER JOIN login l USING(user_id) WHERE ud_id_card != 0 AND l.user_type = 2;";
+$result3 = mysqli_query($conn, $sql3);
+?>
+
+
 <div class="content-wrapper">
   <!-- Content Header (Page header) -->
   <div class="content-header">
     <div class="container-fluid">
       <div class="row mb-2">
         <div class="col-sm-6">
-          <h1 class="m-0">ธนาคาร</h1>
+          <h1 class="m-0">ยืนยันสลีปชำระเงิน</h1>
         </div><!-- /.col -->
         <div class="col-sm-6">
           <ol class="breadcrumb float-sm-right">
@@ -21,61 +33,114 @@
   <div class="content">
     <div class="container-fluid">
       <div class="row">
-        <div class="col-lg-6">
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-
-              <p class="card-text">
-                Some quick example text to build on the card title and make up the bulk of the card's
-                content.
-              </p>
-
-              <a href="#" class="card-link">Card link</a>
-              <a href="#" class="card-link">Another link</a>
-            </div>
-          </div>
-
-          <div class="card card-primary card-outline">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-
-              <p class="card-text">
-                Some quick example text to build on the card title and make up the bulk of the card's
-                content.
-              </p>
-              <a href="#" class="card-link">Card link</a>
-              <a href="#" class="card-link">Another link</a>
-            </div>
-          </div><!-- /.card -->
-        </div>
-        <!-- /.col-md-6 -->
-        <div class="col-lg-6">
-          <div class="card">
+        <div class="col-lg-8">
+          <div class="card card-primary">
             <div class="card-header">
-              <h5 class="m-0">Featured</h5>
+              <h3 class="card-title">ตาราง : รอการตรวจสอบบัตรประชาชน</h3>
             </div>
             <div class="card-body">
-              <h6 class="card-title">Special title treatment</h6>
-
-              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-          </div>
-
-          <div class="card card-primary card-outline">
-            <div class="card-header">
-              <h5 class="m-0">Featured</h5>
-            </div>
-            <div class="card-body">
-              <h6 class="card-title">Special title treatment</h6>
-
-              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
+              <?php if (mysqli_num_rows($result1) > 0) : ?>
+                <table class="table table-bordered">
+                  <thead>
+                    <tr>
+                      <th scope="col">#user_id</th>
+                      <th scope="col">bank</th>
+                      <th scope="col">order_id</th>
+                      <th scope="col">pay_slip</th>
+                      <th scope="col">pay_status</th>
+                      <th scope="col">menu</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <?php foreach ($result1 as $row) : ?>
+                      <tr>
+                        <th scope="row"><?= $row['user_id'] ?></th>
+                        <td><?= $row['user_id'] ?></td>
+                        <td><?= $row['user_id'] ?></td>
+                        <td><?= $row['user_id'] ?></td>
+                        <td><?= $row['user_id'] ?></td>
+                        <td>
+                          <a href="?page=<?= $_GET['page'] ?>&function=detail&user_id=<?= $row['user_id'] ?>" class="btn btn-info" role="button" aria-disabled="true">รายละเอียด</a>
+                        </td>
+                      </tr>
+                    <?php endforeach ?>
+                  </tbody>
+                </table>
+              <?php else : ?>
+                <p class="text-center text-muted mt-3">ไม่มีข้อมูลบัตรประชาชนที่ต้องยืนยันในขณะนี้</p>
+              <?php endif ?>
             </div>
           </div>
         </div>
-        <!-- /.col-md-6 -->
+        <!-- /.col-md-8 -->
+        <div class="col-4">
+          <div class="card card-warning">
+            <div class="card-header">
+              <h3 class="card-title">ตาราง : ภาพไม่สมบูรณ์</h3>
+            </div>
+            <div class="card-body">
+              <?php if (mysqli_num_rows($result2) > 0) : ?>
+                <table class="table table-bordered">
+                  <thead>
+                    <tr>
+                      <th scope="col">#user_id</th>
+                      <th scope="col">bank</th>
+                      <th scope="col">pay_status</th>
+                      <th scope="col">menu</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <?php foreach ($result2 as $row) : ?>
+                      <tr>
+                        <th scope="row"><?= $row['user_id'] ?></th>
+                        <td><?= $row['user_id'] ?></td>
+                        <td><?= $row['user_id'] ?></td>
+                        <td>
+                        <a href="?page=<?= $_GET['page'] ?>&function=detail&user_id=<?= $row['user_id'] ?>" class="btn btn-info" role="button" aria-disabled="true"><i class="fa-solid fa-info"></i></a>
+                        </td>
+                      </tr>
+                    <?php endforeach ?>
+                  </tbody>
+                </table>
+              <?php else : ?>
+                <p class="text-center text-muted mt-3">ไม่พบข้อมูล</p>
+              <?php endif ?>
+            </div>
+          </div>
+          <div class="card card-success">
+            <div class="card-header">
+              <h3 class="card-title">ตาราง : ตรวจสอบเรียบร้อย</h3>
+            </div>
+            <div class="card-body">
+              <?php if (mysqli_num_rows($result3) > 0) : ?>
+                <table class="table table-bordered">
+                  <thead>
+                    <tr>
+                      <th scope="col">#user_id</th>
+                      <th scope="col">bank</th>
+                      <th scope="col">pay_status</th>
+                      <th scope="col">menu</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <?php foreach ($result3 as $row) : ?>
+                      <tr>
+                        <th scope="row"><?= $row['user_id'] ?></th>
+                        <td><?= $row['user_id'] ?></td>
+                        <td><?= $row['user_id'] ?></td>
+                        <td>
+                        <a href="?page=<?= $_GET['page'] ?>&function=detail&user_id=<?= $row['user_id'] ?>" class="btn btn-info" role="button" aria-disabled="true"><i class="fa-solid fa-info"></i></a>
+                        </td>
+                      </tr>
+                    <?php endforeach ?>
+                  </tbody>
+                </table>
+              <?php else : ?>
+                <p class="text-center text-muted mt-3">ไม่พบข้อมูล</p>
+              <?php endif ?>
+            </div>
+          </div>
+        </div>
       </div>
       <!-- /.row -->
     </div><!-- /.container-fluid -->
