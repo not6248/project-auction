41a3bb04c8ee335daadf131283c2601aca773ee0
not6248 <96365700+not6248@users.noreply.github.com>
2023-09-19 00:56:26 +0700

    แก้ไข index

--- admin_panel/page/delivery_type/index.php
+++ admin_panel/page/delivery_type/index.php
@@ -1,3 +1,8 @@
+<?php
+$sql1 = "SELECT * FROM delivery_type";
+$result1 = mysqli_query($conn, $sql1);
+?>
+
 <div class="content-wrapper">
   <!-- Content Header (Page header) -->
   <div class="content-header">
@@ -21,33 +26,44 @@
   <div class="content">
     <div class="container-fluid">
       <div class="row">
-        <div class="col-lg-6">
-          <div class="card">
+        <div class="col-lg-4">
+          <div class="card card-primary">
+            <div class="card-header">
+              <h3 class="card-title">ตาราง : ประเภทขนส่ง</h3>
+            </div>
             <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-
-              <p class="card-text">
-                Some quick example text to build on the card title and make up the bulk of the card's
-                content.
-              </p>
-
-              <a href="#" class="card-link">Card link</a>
-              <a href="#" class="card-link">Another link</a>
+              <div class="text-right">
+                <a href="?page=<?= $_GET['page'] ?>&function=add" class="btn btn-success " role="button"><i class="fa-solid fa-plus"></i> เพิ่มประเภทขนส่ง</a>
+              </div>
+              <?php if (mysqli_num_rows($result1) > 0) : ?>
+                <table class="table table-hover">
+                  <thead>
+                    <tr>
+                      <th scope="col">#dlvt_id</th>
+                      <th scope="col" class="w-75">dlvt_name</th>
+                      <th scope="col">menu</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <?php foreach ($result1 as $row) : ?>
+                      <tr>
+                        <td scope="row"><?= $row['dlvt_id'] ?></td>
+                        <td><?= $row['dlvt_name'] ?></td>
+                        <td class="">
+                          <div class="d-flex ">
+                            <a href="?page=<?= $_GET['page'] ?>&function=update&dlvt_id=<?= $row['dlvt_id'] ?>"><i class="fa-solid fa-pen" style="color:darkorange;"></i></a>
+                            <a href="?page=<?= $_GET['page'] ?>&function=delete&dlvt_id=<?= $row['dlvt_id'] ?>" class="ml-4" onclick="return confirm('คุณต้องการลบประเภทสินค้า <?= $row['dlvt_name'] ?> หรือไม่')"><i class="fa-solid fa-trash" style="color:red;"></i></a>
+                          </div>
+                        </td>
+                      </tr>
+                    <?php endforeach ?>
+                  </tbody>
+                </table>
+              <?php else : ?>
+                <p class="text-center text-muted mt-3">ไม่มีการชำระเงินที่ต้องยืนยันในขณะนี้</p>
+              <?php endif ?>
             </div>
           </div>
-
-          <div class="card card-primary card-outline">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-
-              <p class="card-text">
-                Some quick example text to build on the card title and make up the bulk of the card's
-                content.
-              </p>
-              <a href="#" class="card-link">Card link</a>
-              <a href="#" class="card-link">Another link</a>
-            </div>
-          </div><!-- /.card -->
         </div>
         <!-- /.col-md-6 -->
       </div>
