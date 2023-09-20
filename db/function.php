962d5cac2787ea208f917bd03343e7df6dff492a
not6248 <96365700+not6248@users.noreply.github.com>
2023-09-21 02:14:44 +0700

    การแสดงสถานะ

--- db/function.php
+++ db/function.php
@@ -5,12 +5,24 @@ function echoJson_status_msg($status, $msg)
     echo json_encode(array("status" => $status, "msg" => $msg));
     exit();
 }
-
+// Order Show สถานะ Order ที่แสดงให้ลูกค้าดู
 function os_sts_user($os_id,$os_name_arr){
     if($os_id > 3){
         return $os_name_arr[3];
     }else{
         return $os_name_arr[$os_id];
     }
+}
+
+function order_profile_status($arr,$pay_status,$dlv_status,$u_type){
+        if($pay_status < 3){
+            return $arr[0][$u_type];
+        }elseif($pay_status == 3 && $dlv_status == 0){
+            return $arr[1][$u_type];
+        }elseif($pay_status == 3 && $dlv_status == 1){
+            return $arr[2][$u_type];
+        }elseif($pay_status == 3 && $dlv_status == 2){
+            return $arr[3][$u_type];
+        }
 }
 ?>
