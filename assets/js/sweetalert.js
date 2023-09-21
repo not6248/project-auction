8e800491870dc8d1f2abbed90b96d7afbfe85d2e
not6248 <96365700+not6248@users.noreply.github.com>
2023-09-21 23:20:32 +0700

    ปุ่มยอมรับสินค้า

--- assets/js/sweetalert.js
+++ assets/js/sweetalert.js
@@ -691,3 +691,54 @@ $(document).ready(function () {
           })
     });
 });
+
+$(document).ready(function () {
+    $("#accept-form").submit(function (e) { 
+        e.preventDefault();
+        let Method = $(this).attr("method");
+        let formUrl = $(this).attr("action");
+        let formData = $(this).serialize();
+        console.log(formData);
+        Swal.fire({
+            title: 'คุณแน่ใจหรือไม่?',
+            html: "คุณจะไม่สามารถย้อนกลับสิ่งนี้ได้!<br>กรุณาตรวจสอบสินค้าก่อนกดยืนยันการได้รับสินค้า",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'ใช่! ฉันตรวจสอบแล้ว',
+            cancelButtonText: 'ยกเลิก'
+          }).then((result) => {
+            if (result.isConfirmed) {
+                $.ajax({
+                    type: Method,
+                    url: formUrl,
+                    data: formData,
+                    success: function (response) {
+                        let result = JSON.parse(response)
+                        let status = result.status;
+                        switch (status) {
+                            case "success":
+                                Swal.fire({
+                                    icon: result.status,
+                                    title: result.msg,
+                                    heightAuto: false,
+                                    showConfirmButton: false,
+                                    timer: 1500,
+                                }).then(() => {
+                                    location.reload();
+                                });
+                                break;
+                            case "error":
+                                Swal.fire({
+                                    icon: result.status,
+                                    title: result.msg,
+                                    heightAuto: false,
+                                })
+                        }
+                    }
+                });
+            }
+          })
+    });
+});
