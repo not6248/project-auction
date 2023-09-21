ee3a3369431be5b04d6890dbe553001fee38913b
not6248 <96365700+not6248@users.noreply.github.com>
2023-09-21 23:18:23 +0700

    ajax กรอกเลขขนส่ง

---
+++ ajax/ajax_accept_order.php
@@ -0,0 +1,16 @@
+<?php 
+session_start();
+include '../db/db_conn.php';
+
+if(isset($_POST)){
+    $order_id = $_POST['accept_id'] ??= NULL;
+    $sql = "UPDATE delivery SET dlv_status = '2' WHERE delivery.dlv_id = $order_id ;";
+    $result = mysqli_query($conn,$sql);
+    if($result){
+        echoJson_status_msg("success","ยืนยันสำเร็จ");
+    }else{
+        echoJson_status_msg("error",mysqli_error($conn));
+    }
+    echo  $order_id;
+}
+?>
