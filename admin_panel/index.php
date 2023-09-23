b257dffe36a06d451242007f81d9281221b86e01
not6248 <96365700+not6248@users.noreply.github.com>
2023-09-23 20:15:49 +0700

    เพิ่มการเข้าถึงหน้าต่างๆ

--- admin_panel/index.php
+++ admin_panel/index.php
@@ -21,52 +21,83 @@
       case '':
         include './page/dashboard/index.php';
         break;
+        // ====================================================================================================
       case 'user':
-        include './page/user/index.php';
+        if ($function == 'add') {
+          include './page/user/insert.php';
+        } elseif ($function == 'delete') {
+          include './page/user/delete.php';
+        } elseif ($function == 'update') {
+          include './page/user/edit.php';
+        } elseif ($function == 'detail') {
+          include './page/user/detail.php';
+        } else {
+          include './page/user/index.php';
+        }
         break;
+        // ====================================================================================================
       case 'product':
-        include './page/product/index.php';
+        if ($function == 'add') {
+          include './page/product/insert.php';
+        } elseif ($function == 'delete') {
+          include './page/product/delete.php';
+        } elseif ($function == 'update') {
+          include './page/product/edit.php';
+        } elseif ($function == 'detail') {
+          include './page/product/detail.php';
+        } else {
+          include './page/product/index.php';
+        }
         break;
+        // ====================================================================================================
       case 'order':
-        include './page/order/index.php';
+        if ($function == 'detail') {
+          include './page/order/detail.php';
+        } else {
+          include './page/order/index.php';
+        }
         break;
+        // ====================================================================================================
       case 'prefix':
         include './page/prefix/index.php';
         break;
+        // ====================================================================================================
       case 'product_type':
         if ($function == 'add') {
           include './page/product_type/insert.php';
-        }elseif($function == 'delete'){
+        } elseif ($function == 'delete') {
           include './page/product_type/delete.php';
-        }elseif($function == 'update'){
+        } elseif ($function == 'update') {
           include './page/product_type/edit.php';
-        }else{
-        include './page/product_type/index.php';
+        } else {
+          include './page/product_type/index.php';
         }
         break;
+        // ====================================================================================================
       case 'delivery_type':
         if ($function == 'add') {
           include './page/delivery_type/insert.php';
-        }elseif($function == 'delete'){
+        } elseif ($function == 'delete') {
           include './page/delivery_type/delete.php';
-        }elseif($function == 'update'){
+        } elseif ($function == 'update') {
           include './page/delivery_type/edit.php';
-        }else{
+        } else {
           include './page/delivery_type/index.php';
         }
-
         break;
+        // ====================================================================================================
       case 'bank':
         if ($function == 'add') {
           include './page/bank/insert.php';
-        }elseif($function == 'delete'){
+        } elseif ($function == 'delete') {
           include './page/bank/delete.php';
-        }elseif($function == 'update'){
+        } elseif ($function == 'update') {
           include './page/bank/edit.php';
-        }else{
-        include './page/bank/index.php';
+        } else {
+          include './page/bank/index.php';
         }
         break;
+        // ==================================================================================================== 
       case 'id_card_verified':
         if ($function == 'detail') {
           include './page/id_card_verified/detail.php';
@@ -74,6 +105,7 @@
           include './page/id_card_verified/index.php';
         }
         break;
+        // ====================================================================================================
       case 'receipt_verified':
         if ($function == 'detail') {
           include './page/receipt_verified/detail.php';
@@ -81,6 +113,7 @@
           include './page/receipt_verified/index.php';
         }
         break;
+        // ====================================================================================================
       case 'pay_seller':
         if ($function == 'detail') {
           include './page/pay_seller/detail.php';
@@ -88,6 +121,7 @@
           include './page/pay_seller/index.php';
         }
         break;
+        // ====================================================================================================
       default:
         include './page/not_found.php';
         break;
