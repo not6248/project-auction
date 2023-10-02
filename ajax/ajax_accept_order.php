8f2eaca0ed0cb202c03610d77caca9764276077a
not6248 <96365700+not6248@users.noreply.github.com>
2023-10-03 03:30:53 +0700

    fix bug

--- ajax/ajax_accept_order.php
+++ ajax/ajax_accept_order.php
@@ -2,8 +2,8 @@
 session_start();
 include '../db/db_conn.php';
 
-if(isset($_POST)){
-    $order_id = $_POST['accept_id'] ??= NULL;
+if(isset($_POST) && !empty($_POST)){
+    $order_id = $_POST['accept_id'];
     $sql = "UPDATE delivery SET dlv_status = '2' WHERE delivery.dlv_id = $order_id ;";
     $result = mysqli_query($conn,$sql);
     if($result){
@@ -11,6 +11,7 @@ if(isset($_POST)){
     }else{
         echoJson_status_msg("error",mysqli_error($conn));
     }
-    // echo  $order_id;
+}else{
+    echoJson_status_msg("error","ไม่มีข้อมูล");
 }
 ?>
